094b47ae2028d19de2acbc85ac9e136f643d7094
arpitgupta1906 <gupta.25.iitj.ac.in>
2019-12-17 14:44:40 +0530

    added index.html

--- qnaflow/core/views.py
+++ qnaflow/core/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This should work")
+    return render(request,"index.html")
